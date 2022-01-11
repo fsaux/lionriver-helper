2922e9ba3d2025610919adebcbd0688796bf0a22
Fernando Saux <fernando_saux@hotmail.com>
2022-01-11 18:51:57 -0300

    commit 2

--- index.js
+++ index.js
@@ -1,113 +1,23 @@
 module.exports = function (app) {
-  const logError =
-    app.error ||
-    (err => {
-      console.error(err)
-    })
-  const debug =
-    app.debug ||
-    (msg => {
-      console.log(msg)
-    })
+  var plugin = {};
 
-  var plugin = {
-    unsubscribes: []
-  }
+  plugin.id = 'lionriver-helper';
+  plugin.name = 'Lionriver helper plugin';
+  plugin.description = 'API for Lionriver App towards SK';
 
-  plugin.id = 'set-system-time'
-  plugin.name = 'Set System Time'
-  plugin.description =
-    'Plugin that sets the system date & time from navigation.datetime delta messages'
-
-  plugin.schema = () => ({
-    title: 'Set System Time with sudo',
-    type: 'object',
-    properties: {
-      interval: {
-        type: 'number',
-        title: 'Interval between updates in seconds (0 is once upon plugin start when datetime received)',
-        default: 0
-      },
-      sudo: {
-        type: 'boolean',
-        title: 'Use sudo when setting the time',
-        default: true
-      },
-      preferNetworkTime: {
-        type: 'boolean',
-        title: 'Set system time only if no other source are available ( Only chrony decteded )',
-        default: true
-      }
-    }
-  })
-
-  const SUDO_NOT_AVAILABLE = 'SUDO_NOT_AVAILABLE'
-
-  let count = 0
-  let lastMessage = ''
-  plugin.statusMessage = function () {
-    return `${lastMessage} ${count > 0 ? '- system time set ' + count + ' times' : ''}`
-  }
-
-  plugin.start = function (options) {
-    let stream = app.streambundle.getSelfStream('navigation.datetime')
-    if (options && options.interval > 0) {
-      stream = stream.debounceImmediate(options.interval * 1000)
-    } else {
-      stream = stream.take(1)
-    }
-    plugin.unsubscribes.push(
-      stream.onValue(function (datetime) {
-        var child
-        if (process.platform == 'win32') {
-          console.error("Set-system-time supports only linux-like os's")
-        } else {
-          if( ! plugin.useNetworkTime(options) ){
-            const useSudo = typeof options.sudo === 'undefined' || options.sudo
-            const setDate = `date --iso-8601 -u -s "${datetime}"`
-            const command = useSudo
-              ? `if sudo -n date &> /dev/null ; then sudo ${setDate} ; else exit 3 ; fi`
-              : setDate
-            child = require('child_process').spawn('sh', ['-c', command])
-            child.on('exit', value => {
-              if (value === 0) {
-                count++
-                lastMessage = 'System time set to ' + datetime
-                debug(lastMessage)
-              } else if (value === 3) {
-                lastMessage =
-                  'Passwordless sudo not available, can not set system time'
-                logError(lastMessage)
-              }
-            })
-            child.stderr.on('data', function (data) {
-              lastMessage = data.toString()
-              logError(lastMessage)
-            })
-          }
-        }
-      })
-    )
-  }
-
-  plugin.useNetworkTime = (options) => {
-    if ( typeof options.preferNetworkTime !== 'undefined' && options.preferNetworkTime == true ){
-      const chronyCmd = "chronyc sources 2> /dev/null | cut -c2 | grep -ce '-\|*'";
-      try {
-        validSources = require('child_process').execSync(chronyCmd,{timeout:500});
-      } catch (e) {
-        return false
-      }
-      if(validSources > 0 ){
-        return true
-      }
-    }
-    return false
-  }
+  plugin.start = function (options, restartPlugin) {
+    // Here we put our plugin logic
+    app.debug('Plugin started');
+  };
 
   plugin.stop = function () {
-    plugin.unsubscribes.forEach(f => f())
-  }
+    // Here we put logic we need when the plugin stops
+    app.debug('Plugin stopped');
+  };
 
-  return plugin
-}
+  plugin.schema = {
+    // The plugin schema
+  };
+
+  return plugin;
+};
