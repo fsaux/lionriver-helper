b1d122775af358b160f28df0a58138276e910ceb
Fernando Saux <fernando_saux@hotmail.com>
2022-01-12 13:59:26 -0300

    Put handler for vmg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
-  var plugin = {};
+  var plugin = {}
+  var unsubscribes =[]
 
   plugin.id = 'lionriver-helper';
   plugin.name = 'Lionriver helper plugin';
@@ -8,7 +9,24 @@ module.exports = function (app) {
   plugin.start = function (options, restartPlugin) {
     // Here we put our plugin logic
     app.debug('Plugin started');
+
+    app.registerPutHandler("vessels.self",
+      "navigation.courseGreatCircle.nextPoint.velocityMadeGood",
+      handlePutCourseData)
+
   };
+
+  const handlePutCourseData= (context, path, value, cb) => {
+
+    let val= [ {path, value} ];  
+
+    app.debug(`****** Sending Delta: ******`);
+    app.debug(JSON.stringify(val));
+
+    app.handleMessage(plugin.id, {updates: [ {values: val} ] });
+    return { state: 'COMPLETED', resultStatus: 200, statusCode: 200 } 
+}
+
 
   plugin.stop = function () {
     // Here we put logic we need when the plugin stops
